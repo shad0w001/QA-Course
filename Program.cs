b77e41d3946c9b8694b4ed1c2ce8952e17c463d0
Стилиян Николов <127132509+shad0w001@users.noreply.github.com>
2024-05-25 22:20:07 +0300

    #2 – fixed Console. command order
    And converted the ReadKey() to a ReadLine()

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-    int a = 42;
-    int b = 119;
-    int c = a + b;
-    Console.ReadKey();
-	Console.WriteLine(c);
+int a = 42;
+int b = 119;
+int c = a + b;
+Console.WriteLine(c);
+Console.ReadLine();
+	
